f10d8db22ee5511e74863b9e6d6b2e01eaf04792
KennyReyesS <kennyreyes2000@gmail.com>
2021-10-04 04:56:08 +0000

    height changed in area of my square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -15,7 +15,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.height * self.width
+        return self.width * self.height
 
     def permiterofmysquare(self):
         """ Permiter of my square """
